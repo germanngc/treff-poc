4bc3a98654317ba8b4363bb07b901e2e280ca3ed
German Gonzalez <germanngc@gmail.com>
2025-11-27 16:29:08 -0600

    feat: run database migrations automatically on startup
    Add migration code to Program.cs to ensure database schema is created
    when the application starts. This eliminates the need for manual migrations.

--- apps/backend/WebApi/Program.cs
+++ apps/backend/WebApi/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Context;
 
 namespace WebApi
 {
@@ -13,7 +16,25 @@ namespace WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            
+            // Run migrations on startup
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
